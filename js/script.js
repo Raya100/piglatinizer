4a725348b6597f32f22086d04ada06492463310b
Jaraya Wright <jaraya.wright@greenschoolbrooklyn.com>
2018-01-16 23:30:42 +0000

    changed some stuff

--- js/script.js
+++ js/script.js
@@ -6,17 +6,43 @@
 
 // Document Ready Function. All of your jQuery should go in here. 
 $( document ).ready(function() {
-  
+  $("#button").click(function(){
+        var result= $("input").val();
+  	    // $("#result").text(pigResult);
+  	    
+  	    var words = result.split(" ");
+        var pigResult = ""; 	 
+        for (var i= 0; i < words.length; i++){
+            pigResult = pigResult + wordToPigLatin(words[i] )+ " ";
+        }
+        $("#result").append(pigResult);
 
 
+  });
 
 });
 
 
 // Create the wordToPigLatin function that takes a word as a parameter and returns a transfromed word. 
 
+function wordToPigLatin(word){
 
-
+ var vowels = ["a", "e", "i", "o", "u"]
+ if (v == 'a' || v == 'e' || v == 'i' || v == 'o' || v == 'u')
+ {
+            String convertToPigLatin = findFirstVowel + "ay";
+            System.out.println(convertToPigLatin);
+        }
+        else
+        {
+            String first = findFirstVowel.substring(0,1);
+            String slice = findFirstVowel.substring(1,findFirstVowel.length());
+            System.out.println(slice + first + "ay");
+        }
+    
+ return word + "ay"; 
+ 
+}
 
 
 
@@ -24,4 +50,6 @@ $( document ).ready(function() {
 	//Loops through all the words in the sentence and transforms each word
 	//It should return a transfromed sentance
 
-
+function sentenceToPigLatin(sentence){
+    
+}
